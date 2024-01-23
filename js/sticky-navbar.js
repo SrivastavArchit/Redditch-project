12f7fa38bd8f48fa22ba996b0e3f413fb33fa9fa
SrivastavArchit <architpulkit2003@gmail.com>
2024-01-23 17:59:36 +0530

    first commit

---
+++ js/sticky-navbar.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 160) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
+
+
+/* scrollTop() >= 240
+   Should be equal the the height of the header
+ */
